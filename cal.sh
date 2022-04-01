d94e0091a99e8d963baa599821bcaf93e3e60461
sophie-512 <lys010512@naver.com>
2022-04-01 21:17:45 +0900

    change array in cal.sh

--- cal.sh
+++ cal.sh
@@ -11,20 +11,19 @@ else parameter="$1"
 fi
 
 echo
-num1=`cat num1.txt`
-echo "num1 : " $num1
-num2=`cat num2.txt`
-echo "num2 : " $num2
+num=(`cat num1.txt` `cat num2.txt`)
+echo "num1 : " ${num[0]}
+echo "num2 : " ${num[1]}
 echo "op : " $parameter
 
 case $parameter in
-  "add") let result=`expr $num1 + $num2` 
+  "add") let result=`expr ${num[0]} + ${num[1]}` 
      ;;
-  "sub") let result=`expr $num1 - $num2`
+  "sub") let result=`expr ${num[0]} - ${num[1]}`
      ;;
-  "div") let result=`expr $num1 / $num2`
+  "div") let result=`expr ${num[0]} / ${num[1]}`
      ;;
-  "mul") let result=`expr $num1 \* $num2`
+  "mul") let result=`expr ${num[0]} \* ${num[1]}`
      ;;
 esac
 echo "result : " $result
